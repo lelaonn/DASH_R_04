9a9c4faab4a1924fd8019b63e8105a53451c0438
lelaonn <wesleicbr@gmail.com>
2022-01-17 22:24:55 -0300

    sde_encoder: Remove unnecessary codes

--- drivers/gpu/drm/msm/sde/sde_encoder.c
+++ drivers/gpu/drm/msm/sde/sde_encoder.c
@@ -4834,12 +4834,6 @@ void sde_encoder_kickoff(struct drm_encoder *drm_enc, bool is_error)
 				nsecs_to_jiffies(ktime_to_ns(wakeup_time)));
 	}
 
-	if (drm_enc->bridge && drm_enc->bridge->is_dsi_drm_bridge) {
-		struct dsi_bridge *c_bridge = container_of((drm_enc->bridge), struct dsi_bridge, base);
-		if (c_bridge && c_bridge->display && c_bridge->display->panel)
-			c_bridge->display->panel->kickoff_count++;
-	}
-
 	/*
 	 * Trigger a panel reset if this is the first kickoff and the refresh
 	 * rate is not 60 Hz
